9b649735fd87efbcf912fd2a489f3a1231740e50
Andrew Frueh <andrew.frueh@healthcatalyst.com>
2018-08-11 22:35:53 -0600

    feat(button): adds icon button
    splits icon buttons into a separate example

---
+++ projects/cashmere-examples/src/lib/button-icon-type/button-icon-type-example.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+/**
+ * @title Icon Buttons
+ */
+@Component({
+    selector: 'button-icon-type-example',
+    templateUrl: 'button-icon-type-example.html',
+    styleUrls: ['button-icon-type-example.css']
+})
+export class ButtonIconTypeExample {}
